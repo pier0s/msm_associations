972f827d16820723c000992ef9bdf5e7702494e4
PMatsis <pieros.matsis@users.noreply.github.com>
2016-11-15 01:20:43 -0600

    step 1

---
+++ app/models/movie.rb
@@ -0,0 +1,15 @@
+class Movie < ActiveRecord::Base
+
+  #
+  # - director_id: must be present
+  # - title: must be present; must be unique in combination with year
+  # - year: must be integer between 1870 and 2050
+  # - duration: must be integer between 0 and 2764800
+  # - description: no rules
+  # - image_url: no rules
+
+  validates :director_id, :presence => true
+  validates :title, :presence => true, :uniqueness => { :scope => :year}
+  validates :duration, :numericality => {:only_integer => true, :greater_than_or_equal_to=> 0, :less_than_or_equal_to => 2764800}
+
+end
